ce63b2f0bdf8f17e8377abbd48028ea0c14ac2f5
irfan23m <51937369+irfan23m@users.noreply.github.com>
2019-11-14 11:37:14 +0700

    Last Commit

--- tugasReksa/tugasReksa/settings.py
+++ tugasReksa/tugasReksa/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tabungan'
+    'tabungan',
+    'bootstrap_modal_forms'
 ]
 
 MIDDLEWARE = [
